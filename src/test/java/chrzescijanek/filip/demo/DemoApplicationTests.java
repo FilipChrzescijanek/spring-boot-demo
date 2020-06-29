d2b81e0429fb721556bb45936090e117d2ac1dff
Filip Chrzescijanek <filip.chrzescijanek@gmail.com>
2020-06-29 13:20:39 +0200

    Spring Boot project template

---
+++ src/test/java/chrzescijanek/filip/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package chrzescijanek.filip.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
